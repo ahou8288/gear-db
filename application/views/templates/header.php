940521c65b14c9422913cbdfe49ba1e58ebe4248
Andrew on laptop <houghtonandrew0@gmail.com>
2016-03-21 15:26:09 +1100

    Made datatable work

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,11 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="OPUS IWLR Structure Database">
-    <meta name="author" content="Ben Tudman">
+    <meta name="description" content="Gear DB">
+    <meta name="author" content="Andrew">
     <link rel="icon" href="../../favicon.ico">
-
-    <title>IWLR Structure Database</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -40,28 +38,3 @@
   </head>
 
   <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">IWLR Structure Database</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
-        </div>
-      </div>
-    </nav>
