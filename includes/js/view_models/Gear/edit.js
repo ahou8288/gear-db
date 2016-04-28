0d10d89c85d58c4e48d90416b05d238436a7e40c
Jemma the climber of the south <Jemma@Jemma_PC>
2016-04-27 20:05:46 -0700

    tried to make radio buttons checked on page load.

--- includes/js/view_models/Gear/edit.js
+++ includes/js/view_models/Gear/edit.js
@@ -7,12 +7,34 @@ var ViewModel = function(data){
 
 	self.init = function() {
 		self.generateArrays();
+		self.checkRadio();
 	};
 
 	self.generateArrays = function(){
 		// Put each of the fields in an array.
 		for(var index in data['fields_list']){
 			self.fields_list.push(new RecordViewModel(data['fields_list'][index]));
+		}
+	}
+	self.checkRadio = function(){
+		for(var index in self.initialData.fields_list){
+			if( self.initialData.fields_list[index][2]==1){
+				// console.log(self.initialData.fields_list[index][3]);
+				var f_name = self.initialData.fields_list[index][1];
+				// console.log(f_name);
+				var f_val = self.initialData.fields_list[index][4];
+				var f_num = self.initialData.fields_list[index][3][parseInt(f_val)][0];
+				if (f_name==='type'){
+					f_num=parseInt(f_num)-1;
+				}
+				
+				$(function() {
+					var $radios = $('input:radio[name='+f_name+']');
+					if($radios.is(':checked') === false) {
+						$radios.filter('[value='+f_num+']').prop('checked', true);
+					}
+				});
+			}
 		}
 	}
 }
