b5ea29609257ea824a161b4b18e6c0164c96da2c
Jemma the climber of the south <Jemma@Jemma_PC>
2016-04-30 17:26:36 -0700

    search function in return_table added. Not finished yet

--- application/views/borrow/return_table.php
+++ application/views/borrow/return_table.php
@@ -1,6 +1,18 @@
 <h4>Search for the gear you want to return Gear</h4>
 <h5>Click on gear to return it</h5>
-<table id='dataTable' class='table'></table>
+<table id='dataTable' class='table'>
+	
+
+        <tfoot>
+            <tr>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Name"></th>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Position"></th>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Office"></th>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Age"></th>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Start date"></th>
+            	<th rowspan="1" colspan="1"><input type="text" placeholder="Search Salary"></th></tr>
+        </tfoot>
+</table>
 
 <script>
 	// This script calls the javascript which sets up that dataTable and stuff
--- includes/js/view_models/borrow/return_table.js
+++ includes/js/view_models/borrow/return_table.js
@@ -58,9 +58,35 @@ var ViewModel = function(data){
 		});
 	}
 
+	$(document).ready(function() {
+	    // Setup - add a text input to each footer cell
+	    $('#example tfoot th').each( function () {
+	        var title = $(this).text();
+	        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+	    } );
+	 
+	    // DataTable
+	    var table = self.table
 
+	    console.log(table);
+	 
+	    // Apply the search
+	    table.columns().every( function () {
+	        var that = this;
 
+	        console.log(this);
+	 
+	        $( 'input', this.footer() ).on( 'keyup change', function () {
+	            if ( that.search() !== this.value ) {
+	                that
+	                    .search( this.value )
+	                    .draw();
+	            }
+	        } );
+	    } );
+	} );
 }
+
 
 var RecordViewModel = function(data){
 	var self = this;
